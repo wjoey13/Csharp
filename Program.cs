5afd1d8a18e705fb24b0de6aaa74e7e372b55ebe
joseph walter <joeywalter@josephs-MacBook-Pro.local>
2019-05-20 12:56:40 -0600

    start of calculator app with C#

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace calculaterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string someText = "here is some text";
+            int number = 10;
+            double anotherNumber = 10.5;
+            bool isOrdered = true;
+            string input = Console.ReadLine();
+
+            
+            Console.WriteLine(someText);
+            Console.WriteLine(number);
+            Console.WriteLine(anotherNumber);
+            Console.WriteLine(isOrdered);
+            Console.WriteLine(input);
+            
+
+        }
+    }
+}
